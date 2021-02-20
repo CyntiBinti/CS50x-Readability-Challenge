eab92d38c3b121722a5fe92e14bf04f6d904bc23
Cynthia Mbulu <72040367+CyntiBinti@users.noreply.github.com>
2021-02-20 20:36:27 +0000

    add sentences function

--- readability.c
+++ readability.c
@@ -6,6 +6,10 @@
 int count_letters(string text);
 int count_spaces(string text);
 
+// An array storing each of the text grade options - change 1,2,3 to the actual grade. Will do this later!
+int GRADEarr[] = {1,2,3};
+
+//main function gathers inoout text and outputs letters, words, and sentences. Will finally make it output the grade
 int main(void) {
    
     // Get input text from user to be evaluated
@@ -16,7 +20,7 @@ int main(void) {
 }
     
     // Print the number of letters
-int count_letters(string text) {
+    int count_letters(string text) {
     int letters = 0;
     
     for (int i = 0; i < strlen(text); i++) {
@@ -30,7 +34,7 @@ int count_letters(string text) {
 }
     
     // Print the number of words
-int count_spaces(string text) {
+    int count_spaces(string text) {
     int spaces = 0;
     
     for (int i = 0; i < strlen(text); i++) {
@@ -42,6 +46,16 @@ int count_spaces(string text) {
 }
     
     // Print the number of sentences
+    int count_sentences(string text) {
+    int sentences = 0;
+    
+    for (int i = 0; i < strlen(text); i++) {
+        if ((text[i]) = '.' || '!' || '?') {
+            sentences ++;
+        }
+    }
+    return (sentences);
+}
     
     // Calculate the average number of letters per 100 words in the text (= L)
     
