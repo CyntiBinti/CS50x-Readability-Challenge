fc15231d8f356b72ffc07615ba83083d0553c12d
Cynthia Mbulu <72040367+CyntiBinti@users.noreply.github.com>
2021-02-20 18:22:28 +0000

    Create readability.c

---
+++ readability.c
@@ -0,0 +1,38 @@
+#include <ctype.h>
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int count_letters(string text);
+
+int main(void) {
+   
+    // Get input text from user to be evaluated
+    string text = get_string("Enter text: ");
+    printf("%i\n", count_letters(text));
+    
+}
+    
+    // Print the number of letters
+int count_letters(string text) {
+    int letters = 0;
+    
+    for (int i = 0; i < strlen(text); i++) {
+        if (isupper(text[i])) {
+            letters ++;
+        } else if (islower(text[i])) {
+            letters ++;
+        }
+    }
+    return letters;
+}
+    
+    // Print the number of words
+    
+    // Print the number of sentences
+    
+    // Calculate the average number of letters per 100 words in the text (= L)
+    
+    // Calculate the average number of sentences per 100 words in the text (= S)
+    
+    // Calculate the Coleman-Liau index
